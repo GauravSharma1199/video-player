1f8022ffab789139e2bac3c6e45cf10c3a1d873f
GauravSharma1199 <gaurav25.sharma99@gmail.com>
2026-06-26 18:34:46 +0530

    min

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { useRef } from "react";
 function App() {
   const playerRef = useRef(null);
   const videoLink =
-    "http://localhost:3000/uploads/courses/8e789ef6-2cb4-4c9c-8b59-e908056ce084/index.m3u8";
+    "http://localhost:3000/uploads/courses/04cb5a39-ca11-4b78-abae-eb4f00db9fc7/index.m3u8"
   const videoPlayerOptions = {
     controls: true,
     responsive: true,
